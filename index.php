cbe2ea4c60ec4a98e19ba03b138552d526c31e84
Ryan Garrett <ryangarrettdev@gmail.com>
2020-03-03 16:47:03 +0000

    Test database connection

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php 
-echo "This is the index page.";
-echo "aaaaabb";
+//Test database connection
+
+$db = new PDO( 'sqlite:./sqlite/chi2019.sqlite' );
+
+$sqlQuery = "SELECT name FROM sqlite_master WHERE type='table'";
+
+$stmt = $db->query( $sqlQuery );
+
+while ( $myLine = $stmt->fetchObject()){
+ 
+ echo $myLine->name." ";
+}
+
 ?>
