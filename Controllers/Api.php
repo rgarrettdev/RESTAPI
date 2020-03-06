d9ef039d9f89e87b4b8943aec656fff88f4e3b34
Ryan Garrett <ryangarrettdev@gmail.com>
2020-03-06 17:58:34 +0000

    Implemented query statements/functions

--- Controllers/Api.php
+++ Controllers/Api.php
@@ -1,22 +1,33 @@
 <?php
 class Api extends Controller {
+    //TODO:: ADD COMMENTS ABOUT EACH SELECT STATEMENT!
     public static function printMasterQuery() {
-        echo (Database::query("SELECT name FROM sqlite_master where type='table'"));
+        echo(Database::query("SELECT name FROM sqlite_master where type='table'"));
     }
-    public static function printScheduleQuery() {
-        echo (Database::query("SELECT * FROM 'sessions'"));
+    public static function printScheduleQueryAll() {
+        echo(Database::query("SELECT * FROM 'sessions'"));
+       
     }
-    public static function printScheduleSessionQuery() {
-        print_r(Database::query("SELECT * FROM users"));
+    public static function printScheduleQuerySingle($apiOpt1) {
+        echo(Database::query("SELECT * FROM 'sessions' WHERE id=$apiOpt1"));
+       
     }
-    public static function printPresentationsQuery() {
-        print_r(Database::query("SELECT * FROM users"));
+    public static function printPresentationsQueryAll() {
+        print_r(Database::query("SELECT * FROM activities"));
     }
-    public static function printPresentationsSearchQuery() {
-        print_r(Database::query("SELECT * FROM users"));
+    public static function printShowAllCatrgories() {
+        print_r(Database::query("SELECT DISTINCT type FROM 'sessions'"));
     }
-    public static function printPresentationsCategoryQuery() {
-        print_r(Database::query("SELECT * FROM users"));
+    public static function printPresentationsQuerySearch($apiOpt2) {
+        print_r(Database::query("SELECT * FROM activities WHERE title LIKE '%$apiOpt2%' OR abstract LIKE '%$apiOpt2%'"));
+    }
+    public static function printPresentationsQuerySearchWithCategory($apiOpt2, $apiOpt3) {
+        print_r(Database::query("SELECT e.*, s.type FROM activities e INNER JOIN 'sessions' s ON e.sessionsID=s.id
+         WHERE (e.title LIKE '%$apiOpt2%' OR e.abstract LIKE '%$apiOpt2%') AND s.type='$apiOpt3'"));
+    }
+    public static function printPresentationsQueryCategory($apiOpt2) {
+        print_r(Database::query("SELECT e.*, s.type FROM activities e INNER JOIN 'sessions' s ON e.sessionsID=s.id
+        WHERE s.type='$apiOpt2'"));
     }
 }
 ?>
