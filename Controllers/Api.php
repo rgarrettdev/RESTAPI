647733cbd32ecdd47a118f7136237e4be109eb0c
Ryan Garrett <ryangarrettdev@gmail.com>
2020-03-10 18:10:15 +0000

    Implemented prepared statements

--- Controllers/Api.php
+++ Controllers/Api.php
@@ -9,7 +9,8 @@ class Api extends Controller {
        
     }
     public static function printScheduleQuerySingle($apiOpt1) {
-        echo(Database::query("SELECT * FROM 'sessions' WHERE id=:id",[ ':id' => $apiOpt1 ]));
+        $scheduleID = self::test_input($apiOpt1);
+        echo(Database::query("SELECT * FROM 'sessions' WHERE id=:id",[ ':id' => $scheduleID ]));
        
     }
     public static function printPresentationsQueryAll() {
@@ -18,16 +19,22 @@ class Api extends Controller {
     public static function printShowAllCatrgories() {
         print_r(Database::query("SELECT DISTINCT type FROM 'sessions'"));
     }
-    public static function printPresentationsQuerySearch($apiOpt2) {
-        print_r(Database::query("SELECT * FROM activities WHERE title LIKE '%$apiOpt2%' OR abstract LIKE '%$apiOpt2%'"));
+    public static function printPresentationsQuerySearch($apiOpt1) {
+        $searchType = self::test_input($apiOpt1);
+        print_r(Database::query("SELECT * FROM activities WHERE title LIKE :title OR abstract LIKE :abstract",
+        [ ':title' => '%'.$searchType.'%', ':abstract' => '%'.$searchType.'%'  ]));
     }
-    public static function printPresentationsQuerySearchWithCategory($apiOpt2, $apiOpt3) {
+    public static function printPresentationsQuerySearchWithCategory($apiOpt1, $apiOpt2) {
+        $searchType = self::test_input($apiOpt1);
+        $sessionType = self::test_input($apiOpt2);
         print_r(Database::query("SELECT e.*, s.type FROM activities e INNER JOIN 'sessions' s ON e.sessionsID=s.id
-         WHERE (e.title LIKE '%$apiOpt2%' OR e.abstract LIKE '%$apiOpt2%') AND s.type='$apiOpt3'"));
+         WHERE (e.title LIKE :title OR e.abstract LIKE :abstract) AND s.type=:sessionType",
+          [ ':title' => '%'.$searchType.'%', ':abstract' => '%'.$searchType.'%', ':sessionType' => $sessionType]));
     }
     public static function printPresentationsQueryCategory($apiOpt2) {
+        $sessionType = self::test_input($apiOpt2);
         print_r(Database::query("SELECT e.*, s.type FROM activities e INNER JOIN 'sessions' s ON e.sessionsID=s.id
-        WHERE s.type='$apiOpt2'"));
+        WHERE s.type=:sessionType", [ 'sessionType' => $apiOpt2]));
     }
     public static function loginQuery($loginApiUser,$loginApiPassword) {
         $email = self::test_input($loginApiUser);
