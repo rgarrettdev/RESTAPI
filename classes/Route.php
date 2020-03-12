7ffea76c09c4e9ecdd2b13de732cf4fa87050255
Ryan Garrett <ryangarrettdev@gmail.com>
2020-03-12 15:36:04 +0000

    Added put request

--- classes/Route.php
+++ classes/Route.php
@@ -108,21 +108,39 @@ class Route {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             if ($_GET['url'] == $route && $_GET['api'] == $api && isset($_GET['apiParam1']) == true && sizeof($_GET) == 3) {
                 $apiOpt1 = $_GET['apiParam1'];
-                $loginApiEmail = $_POST['email'];
-                $loginApiPassword = $_POST['password'];
                 $function->__invoke();
                 http_response_code(200);
-                if ($apiOpt1 == NULL && $api == 'login') {
-                    Api::loginQuery($loginApiEmail,$loginApiPassword);
+                if ($api == 'login' && $apiOpt1 == NULL) {
+                    $loginApiEmail = $_POST['email'];
+                    Api::loginRequest($loginApiEmail);
                     http_response_code(200);
-                }  else {
+                } else {
                     echo("ERROR: NOT SUPPORTED POST ENDPOINT");
                     http_response_code(405);
                 }
             }
         }
+        if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+            if ($_GET['url'] == $route && $_GET['api'] == $api && isset($_GET['apiParam1']) == true && sizeof($_GET) == 3) {
+                $apiOpt1 = $_GET['apiParam1'];
+                if ($api == 'schedule' && $apiOpt1 == 'update') {
+                    $_PUT = self::putParse();
+                    Api::updateSessionChair($_PUT['sessionChair']);
+                }
+            }
+        }
     }
     //TODO: ADD CLAUSES FOR OTHER HEADER TYPES!!
+
+    protected static function putParse() {
+        parse_str(file_get_contents("php://input"), $_PUT);
+        foreach ($_PUT as $key => $value) {
+            unset($_PUT[$key]);
+            $_PUT[str_replace('amp;', '', $key)] = $value;
+            
+        }
+        return $_PUT;
+    }
 }
 
 ?>
