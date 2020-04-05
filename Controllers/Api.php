1914df90a7ee937604a9647d8271f051d5bcb535
Ryan Garrett <ryangarrettdev@gmail.com>
2020-04-05 16:02:11 +0100

    Changed static methods to non static

--- Controllers/Api.php
+++ Controllers/Api.php
@@ -4,44 +4,44 @@ class Api extends Controller {
     public static function printMasterQuery() {
         print_r(Database::query("SELECT name FROM sqlite_master where type='table'"));
     }
-    public static function printScheduleQueryAll() {
+    public function printScheduleQueryAll() {
         print_r(Database::query("SELECT * FROM 'sessions'"));
        
     }
-    public static function printScheduleQuerySingle($apiOpt1) {
+    public function printScheduleQuerySingle($apiOpt1) {
         $scheduleID = self::test_input($apiOpt1);
         print_r(Database::query("SELECT * FROM 'sessions' WHERE id=:id",[ ':id' => $scheduleID ]));
        
     }
-    public static function printPresentationsQueryAll() {
+    public function printPresentationsQueryAll() {
         print_r(Database::query("SELECT * FROM activities"));
     }
-    public static function printShowAllCatrgories() {
+    public function printShowAllCatrgories() {
         print_r(Database::query("SELECT DISTINCT type FROM 'sessions'"));
     }
-    public static function printPresentationsQuerySearch($apiOpt1) {
+    public function printPresentationsQuerySearch($apiOpt1) {
         $searchType = self::test_input($apiOpt1);
         print_r(Database::query("SELECT * FROM activities WHERE title LIKE :title OR abstract LIKE :abstract",
         [ ':title' => '%'.$searchType.'%', ':abstract' => '%'.$searchType.'%'  ]));
     }
-    public static function printPresentationsQuerySearchWithCategory($apiOpt1, $apiOpt2) {
+    public function printPresentationsQuerySearchWithCategory($apiOpt1, $apiOpt2) {
         $searchType = self::test_input($apiOpt1);
         $sessionType = self::test_input($apiOpt2);
         print_r(Database::query("SELECT e.*, s.type FROM activities e INNER JOIN 'sessions' s ON e.sessionsID=s.id
          WHERE (e.title LIKE :title OR e.abstract LIKE :abstract) AND s.type=:sessionType",
           [ ':title' => '%'.$searchType.'%', ':abstract' => '%'.$searchType.'%', ':sessionType' => $sessionType]));
     }
-    public static function printPresentationsQueryCategory($apiOpt2) {
+    public function printPresentationsQueryCategory($apiOpt2) {
         $sessionType = self::test_input($apiOpt2);
         print_r(Database::query("SELECT e.*, s.type FROM activities e INNER JOIN 'sessions' s ON e.sessionsID=s.id
         WHERE s.type=:sessionType", [ 'sessionType' => $apiOpt2]));
     }
-    public static function loginRequest($loginApiUser) {
+    public function loginRequest($loginApiUser) {
         $email = self::test_input($loginApiUser);
         print_r(Database::loginRequest("SELECT * FROM users WHERE email=:email",[ ':email' => $email ]));
     }
 
-    public static function updateSessionChair($updateRequestBody, $updateID) {
+    public function updateSessionChair($updateRequestBody, $updateID) {
         $data = self::test_input($updateRequestBody);
         $id = self::test_input($updateID);
         if (!isset($_COOKIE['user'])) {
