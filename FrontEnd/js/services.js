83221774ccafbf0dcd4cc1d899510707bc05ab89
Ryan Garrett <ryangarrettdev@gmail.com>
2020-04-29 16:17:11 +0100

    Added dataservice

--- FrontEnd/js/services.js
+++ FrontEnd/js/services.js
@@ -1,18 +1,29 @@
-app.factory("slotService", [
-  "$cookies",
-  function ($cookies) {
-    var savedData = [];
-    function set(data) {
-      savedData = data;
-      $cookies.put("slotID", savedData);
-    }
-    function get() {
-      return $cookies.get("slotID");
-    }
+app.service("dataService", [
+  "$q",
+  "$http",
+  function ($q, $http) {
+    /**
+     * Var to hold base url
+     */
+    var baseUrl = "http://localhost:8066/api/";
+    /**
+     * method to promise when fulfilled calls the success method.
+     */
+    this.getApiRequest = function (request) {
+      var promise = $q.defer(), //The promise
+        apiRequest = baseUrl + request; //Request
 
-    return {
-      set: set,
-      get: get,
+      $http
+        .get(apiRequest)
+        .then(function (response) {
+          promise.resolve({
+            data: response.data,
+          });
+        })
+        .catch(function (err) {
+          promise.reject(err);
+        });
+      return promise.promise;
     };
   },
 ]);
