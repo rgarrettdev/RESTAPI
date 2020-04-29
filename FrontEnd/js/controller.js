494ff4e6e7f2f58a7b63a40f3e2bd198e480bfcc
Ryan Garrett <ryangarrettdev@gmail.com>
2020-04-29 16:16:58 +0100

    Added dataService to controllers

--- FrontEnd/js/controller.js
+++ FrontEnd/js/controller.js
@@ -1,82 +1,174 @@
+/**
+ * appController, controls the alert bar
+ * gives the user feedback on loading progress,
+ * and to give the user feedback on search functionality.
+ */
 app.controller("appController", [
   "$scope",
   function ($scope) {
     $scope.$on("LOAD", function () {
-      $scope.loading = true;
+      $scope.alert = true;
+      $scope.status = "Loading";
     });
     $scope.$on("UNLOAD", function () {
-      $scope.loading = false;
+      $scope.alert = false;
+    });
+    $scope.$on("SearchNoReturn", function () {
+      $scope.status = "No results found";
     });
   },
 ]);
 
 app.controller("scheduleController", [
   "$scope",
-  "$http",
-  "slotService",
-  function ($scope, $http, slotService) {
+  "dataService",
+  function ($scope, dataService) {
     $scope.$emit("LOAD");
-    $http
-      .get("http://localhost:8066/api/schedule/")
-      .then(function (response) {
-        $scope.schedule = response.data;
-      })
-      .finally(function () {
-        $scope.$emit("UNLOAD");
-      });
-    $scope.addSlotsID = function (slot) {
-      $scope.slotsID = slot;
-      slotService.set($scope.slotsID);
+    var getSchedule = function (request) {
+      dataService
+        .getApiRequest(request)
+        .then(
+          function (response) {
+            $scope.schedule = response.data;
+          },
+          function (err) {
+            $scope.status = "Unable to load data " + err;
+          },
+          function (notify) {
+            console.log(notify);
+          }
+        )
+        .finally(function () {
+          $scope.$emit("UNLOAD");
+        });
     };
+    getSchedule("schedule/");
   },
 ]);
 
 app.controller("scheduleDetailedController", [
   "$scope",
-  "$http",
-  "slotService",
-  function ($scope, $http, slotService) {
-    $scope.slotIDs = slotService.get();
+  "dataService",
+  "$routeParams",
+  function ($scope, dataService, $routeParams) {
     $scope.$emit("LOAD");
-    if (!$scope.slotIDs) {
-      console.log("Please select a timeslot on the landing page");
-    } else {
-      $http
-        .get("http://localhost:8066/api/schedule/" + $scope.slotIDs)
-        .then(function (response) {
-          $scope.schedule = response.data;
-        })
+    var getScheduleDetailed = function (request) {
+      dataService
+        .getApiRequest(request)
+        .then(
+          function (response) {
+            $scope.schedule = response.data;
+          },
+          function (err) {
+            $scope.status = "Unable to load data " + err;
+          },
+          function (notify) {
+            console.log(notify);
+          }
+        )
         .finally(function () {
           $scope.$emit("UNLOAD");
         });
-    }
+    };
+    getScheduleDetailed("schedule/" + $routeParams.id);
   },
 ]);
 
 app.controller("presentationController", [
   "$scope",
-  "$http",
-  function ($scope, $http, ) {
+  "dataService",
+  function ($scope, dataService) {
     $scope.$emit("LOAD");
-    $http
-      .get("http://localhost:8066/api/presentations/")
-      .then(function (response) {
-        $scope.presentations = response.data;
-      })
-      .finally(function () {
-        $scope.$emit("UNLOAD");
-      });
-  }
+    var getPresentation = function (request) {
+      dataService
+        .getApiRequest(request)
+        .then(
+          function (response) {
+            $scope.presentations = response.data;
+          },
+          function (err) {
+            $scope.status = "Unable to load data " + err;
+          },
+          function (notify) {
+            console.log(notify);
+          }
+        )
+        .finally(function () {
+          $scope.$emit("UNLOAD");
+        });
+    };
+    getPresentation("presentations/");
+
+    $scope.resetPos = function () {
+      window.scrollTo(0, 0);
+    };
+  },
 ]);
 
-app.controller("presentationSearchController",["$scope","$http","$routeParams", function ($scope, $http, $routeParams) {
-  $scope.$emit("LOAD");
-    $http
-      .get("http://localhost:8066/api/presentations/search/" + $routeParams.term)
-      .then(function (response) {
-        $scope.search = response.data;
-      })
-      .finally(function () {
-        $scope.$emit("UNLOAD");
-      });
-}]);
+app.controller("presentationDetailedController", [
+  "$scope",
+  "dataService",
+  "$routeParams",
+  function ($scope, dataService, $routeParams) {
+    $scope.$emit("LOAD");
+    var getScheduleDetailed = function (request) {
+      dataService
+        .getApiRequest(request)
+        .then(
+          function (response) {
+            $scope.presentations = response.data;
+          },
+          function (err) {
+            $scope.status = "Unable to load data " + err;
+          },
+          function (notify) {
+            console.log(notify);
+          }
+        )
+        .finally(function () {
+          $scope.$emit("UNLOAD");
+        });
+    };
+    getScheduleDetailed("presentations/" + $routeParams.id);
+
+    $scope.resetPos = function () {
+      window.scrollTo(0, 0);
+    };
+  },
+]);
+
+app.controller("presentationSearchController", [
+  "$scope",
+  "dataService",
+  "$routeParams",
+  function ($scope, dataService, $routeParams) {
+    $scope.$emit("LOAD");
+    var getPresentationSearch = function (request) {
+      dataService
+        .getApiRequest(request)
+        .then(
+          function (response) {
+            $scope.search = response.data;
+          },
+          function (err) {
+            $scope.status = "Unable to load data " + err;
+          },
+          function (notify) {
+            console.log(notify);
+          }
+        )
+        .finally(function () {
+          if ($scope.search.length > 0) {
+            $scope.$emit("UNLOAD");
+          } else {
+            $scope.$emit("SearchNoReturn");
+          }
+        });
+    };
+    getPresentationSearch("presentations/search/" + $routeParams.term);
+
+    $scope.resetPos = function () {
+      window.scrollTo(0, 0);
+    };
+  },
+]);
